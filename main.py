599768730cb2d979678860bd410b5bfb9d9c729d
Kamila Wisniewska <kmlwisniewska@gmail.com>
2022-09-29 12:37:45 -0400

    Improved documentation to ensure easy use and editing.

--- main.py
+++ main.py
@@ -4,28 +4,30 @@ This application allows the user to generate new passwords, and store their new 
 passwords as a .txt file.
 
 To personalize the file, you can uncomment the EMAIL constant variable, save your email address
-to is as a string. Next, uncomment line 52 - under the comment '# Uncomment following row if you
+to is as a string. Next, uncomment line 77 - under the comment '# Uncomment following row if you
 would like a constant EMAIL to be populated in Email/Username'
 
-This application utilizes the `tkinter` module and `messagebox` from `tkinter`.
-
+This application utilizes the `tkinter` module, `messagebox` from `tkinter` and 
+`password_generator` module.
 """
 from tkinter import *
 from tkinter import messagebox
 from password_generator import password_generator
+#import pyperclip
 
 # Uncomment EMAIL constant and change to your email if you would like to autopopulate your email
-EMAIL = "kamila@gmail.com"
+#EMAIL = "kamila@gmail.com"
 
-# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
 def generate_password():
+    """Calls password_generator from password_generator module. Generates new randomly
+    generated password and inserts it into the password Entry field."""
     if len(password.get()) == 0:
-        password.insert(0, password_generator())  
+        password.insert(0, password_generator())
 
-# ---------------------------- SAVE PASSWORD ------------------------------- #
 def save_password():
     """Saves the current information to data.txt."""
-    # Ask user if the information entered is correct.
+    # Ask user if the information entered is correct to ensure they want to save.
     is_correct = messagebox.askokcancel(title=website, message=f"Saving these details:\nEmail: "
     f"{email_username.get()}\nPassword: {password.get()}\nWebsite: {website.get()}\n OK to save?")
     # Save information to file.
@@ -43,17 +45,17 @@ def complete_input():
     else:
         save_password()
 
-# ---------------------------- UI SETUP ------------------------------- #
-# Create a window for application.
+# Create a window for application interface.
 window = Tk()
 window.title("Password Manager")
+# Sets padding to 50 on both x and y axis.
 window.config(padx=50, pady=50)
 
 # Create canvas for window.
 canvas = Canvas(width=200, height=200)
 # Create PhotoImage in order to use image on canvas.
 logo = PhotoImage(file="logo.png")
-# Adds logo image to canvas and displays it on the window.
+# Adds logo image to canvas and displays it in the center of the canvas.
 canvas.create_image(100, 100, image=logo)
 canvas.grid(row=0, column=1)
 
@@ -72,7 +74,7 @@ website.focus()
 email_username = Entry(width=35)
 email_username.grid(row=2, column=1, columnspan=2)
 # Uncomment following row if you would like a constant EMAIL to be populated in Email/Username
-email_username.insert(0, EMAIL)
+#email_username.insert(0, EMAIL)
 password = Entry(width=18)
 password.grid(row=3, column=1)
 
--- password_generator.py
+++ password_generator.py
@@ -1,4 +1,19 @@
-"""Password Generator"""
+"""Password Generator module for Password Manager.
+
+This module returns a randomly generated password consisting of: 8-10 letters, 2-4 symbols,
+and 2-4 numbers that are shuffled at random. This module was created to be used with the Password
+Manager app.
+
+Example Output:
+    "PM6KG5Nz0+%&0zukR"
+    "Z*Y90*(CgOuNX&"
+
+Attributes:
+    letters (list): list of (str) containing all letters that can be included in the new password.
+    numbers (list): list of (int) containing numbers from 0 - 9.
+    symboles (list): list of (str) containing all symbols that can be included in password.
+
+"""
 from random import choice, randint, shuffle
 
 letters = [

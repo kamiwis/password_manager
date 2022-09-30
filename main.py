30886657c9e26a27e6c0fa82ebef659fb20192e0
Kamila Wisniewska <kmlwisniewska@gmail.com>
2022-09-29 21:24:53 -0400

    Made color changes to ensure app can be used in dark mode.

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 """Password Manager
 
-This application allows the user to generate new passwords, and store their new or existing 
+This application allows the user to generate new passwords, and store their new or existing
 passwords as a .txt file.
 
 To personalize the file, you can uncomment the EMAIL constant variable, save your email address
 to is as a string. Next, uncomment line 77 - under the comment '# Uncomment following row if you
 would like a constant EMAIL to be populated in Email/Username'
 
-This script requires that `pyperclip` be installed within the Python environment you are 
+This script requires that `pyperclip` be installed within the Python environment you are
 running this script in.
 """
 from tkinter import *
@@ -34,12 +34,7 @@ def save_password():
     website_data = website.get()
     email_data = email_username.get()
     password_data = password.get()
-    new_data = {
-        website_data: {
-            "email": email_data,
-            "password": password_data
-        }
-    }
+    new_data = {website_data: {"email": email_data, "password": password_data}}
 
     if len(website.get()) == 0 or len(password.get()) == 0:
         messagebox.showinfo(title="Oops", message="Please complete all fields")
@@ -61,8 +56,9 @@ def save_password():
         finally:
             website.delete(0, END)
             password.delete(0, END)
-    
+
 def retrieve_data():
+    """Retrieves data from .json file and returns to user."""
     website_to_find = website.get()
     try:
         # Open json file
@@ -74,18 +70,24 @@ def retrieve_data():
         if website_to_find in data:
             email_data = data[website_to_find]["email"]
             password_data = data[website_to_find]["password"]
-            messagebox.showinfo(title=website_to_find, message=f"Email: {email_data} \nPassword: {password_data}")
+            messagebox.showinfo(
+                title=website_to_find,
+                message=f"Email: {email_data} \nPassword: {password_data}")
         else:
-            messagebox.showinfo(title="Error", message=f"No details found for {website_to_find}")
+            messagebox.showinfo(
+                title="Error",
+                message=f"No details found for {website_to_find}")
+
 
 # Create a window for application interface.
 window = Tk()
 window.title("Password Manager")
 # Sets padding to 50 on both x and y axis.
 window.config(padx=50, pady=50)
+window.config(bg="white")
 
 # Create canvas for window.
-canvas = Canvas(width=200, height=200)
+canvas = Canvas(width=200, height=200, bg="white", highlightthickness=0)
 # Create PhotoImage in order to use image on canvas.
 logo = PhotoImage(file="logo.png")
 # Adds logo image to canvas and displays it in the center of the canvas.
@@ -93,30 +95,41 @@ canvas.create_image(100, 100, image=logo)
 canvas.grid(row=0, column=1)
 
 # Create labels for input fields and place in grid.
-website_label = Label(text="Website:")
+website_label = Label(text="Website:", bg="white", fg="black")
 website_label.grid(row=1, column=0)
-email_username_label = Label(text="Email/Username:")
+email_username_label = Label(text="Email/Username:", bg="white", fg="black")
 email_username_label.grid(row=2, column=0)
-password_label = Label(text="Password:")
+password_label = Label(text="Password:", bg="white", fg="black")
 password_label.grid(row=3, column=0)
 
 # Create input fields and place in grid.
-website = Entry(width=18)
+website = Entry(width=18, bg="white", fg="black", bd=0)
 website.grid(row=1, column=1)
 website.focus()
-email_username = Entry(width=35)
+email_username = Entry(width=35, bg="white", fg="black", bd=0)
 email_username.grid(row=2, column=1, columnspan=2)
 # Uncomment following row if you would like a constant EMAIL to be populated in Email/Username
 #email_username.insert(0, EMAIL)
-password = Entry(width=18)
+password = Entry(width=18, bg="white", fg="black", bd=0)
 password.grid(row=3, column=1)
 
 # Create buttons and place in grid.
-generate_button = Button(text="Generate Password", command=generate_password)
+generate_button = Button(text="Generate Password",
+                         command=generate_password,
+                         fg="black",
+                         highlightbackground="white")
 generate_button.grid(row=3, column=2)
-add_button = Button(text="Add", width=36, command=save_password)
+add_button = Button(text="Add",
+                    width=36,
+                    command=save_password,
+                    fg="black",
+                    highlightbackground="white")
 add_button.grid(row=4, column=1, columnspan=2)
-search_button = Button(text="Search", width=13, command=retrieve_data)
+search_button = Button(text="Search",
+                       width=13,
+                       command=retrieve_data,
+                       fg="black",
+                       highlightbackground="white")
 search_button.grid(row=1, column=2)
 
 window.mainloop()
